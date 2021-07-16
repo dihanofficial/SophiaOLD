1e442df5a15f55f02d1d16e88acbc804eabecf16
Dihan Randila <84424959+dihanrandila1@users.noreply.github.com>
2021-07-16 16:28:35 +0530

    Update __init__.py

--- Sophia/__init__.py
+++ Sophia/__init__.py
@@ -203,7 +203,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from AnkiVector.modules.helper_funcs.handlers import (
+from Sophia.modules.helper_funcs.handlers import (
     CustomCommandHandler,
     CustomMessageHandler,
     CustomRegexHandler,
