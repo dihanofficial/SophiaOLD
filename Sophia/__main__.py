7153ab51a2e87cd223b018dbae1e17235892ff6a
Dihan Randila <84424959+dihanrandila1@users.noreply.github.com>
2021-07-16 07:00:01 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -1,1 +1,27 @@
+import importlib
+import time
+import re
+from sys import argv
+from typing import Optional
+
+from pyrogram import filters, idle
+
+from Sophia import (
+    ALLOW_EXCL,
+    CERT_PATH,
+    DONATION_LINK,
+    LOGGER,
+    OWNER_ID,
+    PORT,
+    SUPPORT_CHAT,
+    TOKEN,
+    URL,
+    WEBHOOK,
+    SUPPORT_CHAT,
+    dispatcher,
+    StartTime,
+    telethn,
+    pbot,
+    updater,
+)
 
