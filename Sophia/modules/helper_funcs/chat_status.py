c8c56042d4a5c3965e9b76a09ef92ef4eef5b335
Dihan Randila <84424959+dihanrandila1@users.noreply.github.com>
2021-07-16 20:54:41 +0530

    Update chat_status.py

--- Sophia/modules/helper_funcs/chat_status.py
+++ Sophia/modules/helper_funcs/chat_status.py
@@ -402,6 +402,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from Sophiat.modules import connection
+from Sophia.modules import connection
 
 connected = connection.connected
