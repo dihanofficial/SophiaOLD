70ea136d4fbabef323f84ab8184077a6f89e0a3c
Dihan Randila <84424959+dihanrandila1@users.noreply.github.com>
2021-07-17 06:51:35 +0530

    Update connection.py

--- Sophia/modules/connection.py
+++ Sophia/modules/connection.py
@@ -387,7 +387,7 @@ def connect_button(update, context):
         connect_chat(update, context)
 
 
-__mod_name__ = "⚡️Connect⚡️"
+__mod_name__ = "💞Connect💞"
 
 __help__ = """
 Sometimes, you just want to add some notes and filters to a group chat, but you don't want everyone to see; This is where connections come in...
