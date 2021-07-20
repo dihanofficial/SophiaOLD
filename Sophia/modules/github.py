cbdc36dabd881b11d4252061df3a7d6464857df5
Dihan Randila <84424959+dihanrandila1@users.noreply.github.com>
2021-07-20 11:27:13 +0530

    Update github.py

--- Sophia/modules/github.py
+++ Sophia/modules/github.py
@@ -1,7 +1,7 @@
 import aiohttp
 from pyrogram import filters
 from Sophia import pbot
-from Sophia.pyrogramee.errors import capture_err
+from Sophia.Best_Of_Sophia.errors import capture_err
 
 
 __mod_name__ = "💞Github💞"
