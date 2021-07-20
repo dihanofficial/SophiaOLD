4478eedd85983c0b71bc8d89ed5ece9b38466d19
Dihan Randila <84424959+dihanrandila1@users.noreply.github.com>
2021-07-20 11:41:18 +0530

    Update pyrogram.py

--- Sophia/Best_Of_Sophia/pyrogram.py
+++ Sophia/Best_Of_Sophia/pyrogram.py
@@ -3,7 +3,7 @@ import logging
 from pyrogram import Client
 
 # from pyromod import listen
-from HexzyBot.conf import get_int_key, get_str_key
+from Sophia.conf import get_int_key, get_str_key
 
 TOKEN = get_str_key("TOKEN", required=True)
 API_ID = get_int_key("API_ID", required=True)
