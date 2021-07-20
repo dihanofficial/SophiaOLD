200f9c73dc7f7e57d51a545090e551fefd276bb0
Dihan Randila <84424959+dihanrandila1@users.noreply.github.com>
2021-07-20 12:23:08 +0530

    Update shield.py

--- Sophia/modules/shield.py
+++ Sophia/modules/shield.py
@@ -16,7 +16,7 @@ from Sophia import BOT_ID
 from Sophia.conf import get_int_key, get_str_key
 
 
-from Sophia.Best_of_Sophia.telethonbasics import is_admin
+from Sophia.Best_Of_Sophia.telethonbasics import is_admin
 from Sophia.events import register
 from Sophia import MONGO_DB_URI 
 from pymongo import MongoClient
