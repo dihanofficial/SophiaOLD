ac2c6ca876bafb112b2d33c779d04605e9e4b849
Dihan Randila <84424959+dihanrandila1@users.noreply.github.com>
2021-07-21 10:56:59 +0530

    Update Logo.py

--- Sophia/modules/Logo.py
+++ Sophia/modules/Logo.py
@@ -90,63 +90,23 @@ async def lego(event):
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
-    fillcolor = "red"
+    fillcolor = "yello"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./Sophia/etc/Font.ttf", 330)
+    font = ImageFont.truetype("./Sophia/etc/Maghrib.ttf", 1000)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
     draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="red")
+    draw.text((x, y), text, font=font, fill="yello", stroke_width=0, stroke_fill="yello")
     fname2 = "SophiaLogo.png"
     img.save(fname2, "png")
     await tbot.send_file(event.chat_id, fname2, caption="Made By @SophiaSLBot 💞")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Go to Help From  @SophiaX_Support, {e}')
-
-    
-
-   
-@register(pattern="^/ylogo ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
-     
-    if not quew:
-       await event.reply('Provide Some Text To Draw!')
-       return
-    else:
-       pass
- await event.reply('Creating your logo...')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open('./Sophia/etc/blackbg.jpg')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "gold"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./Sophia/etc/Font.ttf", 330)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yelllo")
-    fname2 = "SophiaLogo.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @SophiaSLBot 💞")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Error Go to Help From  @SophiaX_Support, {e}')
+   await event.reply(f'Error Go to Help From  @Dihan_Official, {e}')
 
 
 
@@ -186,7 +146,51 @@ async def lego(event):
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Go to Help From  @SophiaX_Support, {e}')
+   await event.reply(f'Error Go to Help From  @Dihan_Official, {e}')
+
+
+
+
+@register(pattern="^/rlogo ?(.*)")
+async def lego(event):
+ quew = event.pattern_match.group(1)
+ if event.sender_id == OWNER_ID:
+     pass
+ else:
+     
+    if not quew:
+       await event.reply('Provide Some Text To Draw!')
+       return
+    else:
+       pass
+ await event.reply('Creating your logo...')
+ try:
+    text = event.pattern_match.group(1)
+    img = Image.open('./Sophia/etc/blackbg.jpg')
+    draw = ImageDraw.Draw(img)
+    image_widthz, image_heightz = img.size
+    pointsize = 500
+    fillcolor = "red"
+    shadowcolor = "blue"
+    font = ImageFont.truetype("./Sophia/etc/Maghrib.ttf", 1000)
+    w, h = draw.textsize(text, font=font)
+    h += int(h*0.21)
+    image_width, image_height = img.size
+    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
+    x = (image_widthz-w)/2
+    y= ((image_heightz-h)/2+6)
+    draw.text((x, y), text, font=font, fill="red", stroke_width=0, stroke_fill="red")
+    fname2 = "SophiaLogo.png"
+    img.save(fname2, "png")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By @SophiaSLBot")
+    if os.path.exists(fname2):
+            os.remove(fname2)
+ except Exception as e:
+   await event.reply(f'Error Go to Help From  @Dihan_Official, {e}')
+
+
+
+
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
@@ -194,8 +198,8 @@ file_helpo = file_help.replace("_", " ")
 
 
 __help__ = """
- ~ /logo {text} :  Create your Red logo with your name
- ~ /ylogo {text} :  Create your Yello Logo with your name
- ~ /wlogo {text} :  Create your White logo with your name
+ ~ /logo {text} :  Create your Yello logo with your name
+ ~ /wlogo {text} :  Create your White Logo with your name
+ ~ /rlogo {text} :  Create your Red logo with your name
  """
 __mod_name__ = "💞Logo💞"
