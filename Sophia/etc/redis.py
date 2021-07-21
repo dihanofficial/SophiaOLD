bb2047da5d55b69b65592c1f2e682184e432879e
Dihan Randila <84424959+dihanrandila1@users.noreply.github.com>
2021-07-21 06:20:53 +0530

    Sophia

---
+++ Sophia/etc/redis.py
@@ -0,0 +1,25 @@
+import sys
+
+import redis as redis_lib
+
+from Sophia import log
+from Sophia.config import get_str_key
+
+# Init Redis
+redis = redis_lib.Redis(
+    host=get_str_key("REDIS_URI"),
+    port=get_str_key("REDIS_PORT"),
+    password=get_str_key("REDIS_PASS"),
+    decode_responses=True,
+)
+
+bredis = redis_lib.Redis(
+    host=get_str_key("REDIS_URI"),
+    port=get_str_key("REDIS_PORT"),
+    password=get_str_key("REDIS_PASS"),
+)
+
+try:
+    redis.ping()
+except redis_lib.ConnectionError:
+    sys.exit(log.critical("Can't connect to RedisDB! Exiting..."))
