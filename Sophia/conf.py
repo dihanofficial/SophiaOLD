3a430869f05ad050bc9f0b6d25994a8a9951db8b
Dihan Randila <84424959+dihanrandila1@users.noreply.github.com>
2021-07-21 10:30:17 +0530

    Update conf.py

--- Sophia/conf.py
+++ Sophia/conf.py
@@ -1,9 +1,9 @@
 import os
-
-
 import sys
+
+import yaml
 from envparse import env
-from Sophia import LOGGER
+
 from Sophia.utils.logger import log
 
 DEFAULTS = {
@@ -31,36 +31,6 @@ if os.path.isfile(CONFIG_PATH):
         DEFAULTS[item.upper()] = data[item]
 else:
     log.info("Using env vars")
-
-
-
-def get_str_key(name, required=False):
-    if name in DEFAULTS:
-        default = DEFAULTS[name]
-    else:
-        default = None
-    if not (data := env.str(name, default=default)) and not required:
-        LOGGER.warn("No str key: " + name)
-        return None
-    elif not data:
-        LOGGER.critical("No str key: " + name)
-        sys.exit(2)
-    else:
-        return data
-
-def get_int_key(name, required=False):
-    if name in DEFAULTS:
-        default = DEFAULTS[name]
-    else:
-        default = None
-    if not (data := env.int(name, default=default)) and not required:
-        LOGGER.warn("No int key: " + name)
-        return None
-    elif not data:
-        LOGGER.critical("No int key: " + name)
-        sys.exit(2)
-    else:
-        return data
 
 
 def get_str_key(name, required=False):
