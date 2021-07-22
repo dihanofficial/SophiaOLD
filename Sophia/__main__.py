177f74523422a94465dd939aabb97ab44baa4534
Dihan Randila <84424959+dihanrandila1@users.noreply.github.com>
2021-07-22 21:50:18 +0530

    Update __main__.py

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -1,90 +1,112 @@
-import asyncio
-import os
-from importlib import import_module
+import importlib
+import time
+import re
+from sys import argv
+from typing import Optional
+from pyrogram import filters, idle
 
-from aiogram import executor
-from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from Sophia import (
+    ALLOW_EXCL,
+    CERT_PATH,
+    DONATION_LINK,
+    LOGGER,
+    OWNER_ID,
+    PORT,
+    SUPPORT_CHAT,
+    TOKEN,
+    URL,
+    WEBHOOK,
+    SUPPORT_CHAT,
+    dispatcher,
+    StartTime,
+    telethn,
+    pbot,
+    updater,
+)
 
-from Sophia import TOKEN, bot, dp
-from Sophia.config import get_bool_key, get_list_key
-from Sophia.modules import ALL_MODULES, LOADED_MODULES, MOD_HELP
-from Sophia.utils.logger import log
-
-if get_bool_key("DEBUG_MODE"):
-    log.debug("Enabling logging middleware.")
-    dp.middleware.setup(LoggingMiddleware())
-
-LOAD = get_list_key("LOAD")
-DONT_LOAD = get_list_key("DONT_LOAD")
-
-if get_bool_key("LOAD_MODULES"):
-    if len(LOAD) > 0:
-        modules = LOAD
-    else:
-        modules = ALL_MODULES
-
-    modules = [x for x in modules if x not in DONT_LOAD]
-
-    log.info("Modules to load: %s", str(modules))
-    for module_name in modules:
-        # Load pm_menu at last
-        if module_name == "pm_menu":
-            continue
-        log.debug(f"Importing <d><n>{module_name}</></>")
-        imported_module = import_module("Sophia.modules." + module_name)
-        if hasattr(imported_module, "__help__"):
-            if hasattr(imported_module, "__mod_name__"):
-                MOD_HELP[imported_module.__mod_name__] = imported_module.__help__
-            else:
-                MOD_HELP[imported_module.__name__] = imported_module.__help__
-        LOADED_MODULES.append(imported_module)
-    log.info("Modules loaded!")
-else:
-    log.warning("Not importing modules!")
-
-loop = asyncio.get_event_loop()
-
-import_module("Sophia.modules.pm_menu")
-# Import misc stuff
-import_module("Sophia.utils.exit_gracefully")
-if not get_bool_key("DEBUG_MODE"):
-    import_module("Sophia.utils.sentry")
-
-
-async def before_srv_task(loop):
-    for module in [m for m in LOADED_MODULES if hasattr(m, "__before_serving__")]:
-        log.debug("Before serving: " + module.__name__)
-        loop.create_task(module.__before_serving__(loop))
+# needed to dynamically load modules
+# NOTE: Module order is not guaranteed, specify that in the config file!
+from Sophia.modules import ALL_MODULES
+from Sophia.modules.helper_funcs.chat_status import is_user_admin
+from Sophia.modules.helper_funcs.misc import paginate_modules
+from Sophia.modules.sudoers import bot_sys_stats
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
+from telegram.error import (
+    BadRequest,
+    ChatMigrated,
+    NetworkError,
+    TelegramError,
+    TimedOut,
+    Unauthorized,
+)
+from telegram.ext import (
+    CallbackContext,
+    CallbackQueryHandler,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+)
+from telegram.ext.dispatcher import DispatcherHandlerStop, run_async
+from telegram.utils.helpers import escape_markdown
 
 
-async def start(_):
-    log.debug("Starting before serving task for all modules...")
-    loop.create_task(before_srv_task(loop))
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
 
-    if not get_bool_key("DEBUG_MODE"):
-        log.debug("Waiting 2 seconds...")
-        await asyncio.sleep(2)
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
 
 
-async def start_webhooks(_):
-    url = os.getenv("WEBHOOK_URL") + f"/{TOKEN}"
-    await bot.set_webhook(url)
-    return await start(_)
+PM_START_TEXT = f"""
+Hey there! My name is Sophia.
+I can help manage your groups with useful features, feel free to add me to your Groups!  *Hit /help 
+Sophia Updates @dihanofficial 
+"""
+
+buttons = [
+    [
+        InlineKeyboardButton(
+            text="❓ Help", callback_data="help_back"),
+    ],
+    [
+        InlineKeyboardButton(text="🙋‍♀️ Sophia Updates", callback_data="t.me/dihanofficial"),
+        InlineKeyboardButton(
+            text="🙋‍♂️ Sophia Support", url=f"https://t.me/dihan_official"
+        ),
+    ],
+    [
+        InlineKeyboardButton(text="👨‍🔧 Sophia Logs", callback_data="t.me/SophiaX_Updates"),
+        InlineKeyboardButton(
+            text="🤴 Developer", url=f"https://t.me/dihanrandila"
+        ),
+    ],
+    [
+        InlineKeyboardButton(text="🛠 Source Code ", url=f"https://github.com/dihanrandila"),
+        InlineKeyboardButton(
+            text="💾 System Stats", callback_data="stats_callback"
+        ),
+    ],
+    [
+        InlineKeyboardButton(text="➕ Add Sophia to your Group ➕", url="t.me/SophiaSLBot?startgroup=true"),
+    ],
+]
 
 
-log.info("Starting loop..")
-log.info("Aiogram: Using polling method")
-
-if os.getenv("WEBHOOKS", False):
-    port = os.getenv("WEBHOOKS_PORT", 8080)
-    executor.start_webhook(dp, f"/{TOKEN}", on_startup=start_webhooks, port=port)
-else:
-    executor.start_polling(
-        dp,
-        loop=loop,
-        on_startup=start,
-        timeout=15,
-        relax=0.1,
-        fast=True,
-        skip_updates=True,
-    )
