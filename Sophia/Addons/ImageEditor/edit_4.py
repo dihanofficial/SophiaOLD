e8255d334ba26cde0c6711ad6f91f363fd0643ae
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-07-26 16:06:15 +0530

    Sophia

--- Sophia/Addons/ImageEditor/edit_4.py
+++ Sophia/Addons/ImageEditor/edit_4.py
@@ -8,7 +8,7 @@ import numpy as np
 import requests
 from PIL import Image, ImageDraw, ImageOps
 
-from HexzyBot.conf import get_str_key
+from Sophia.conf import get_str_key
 
 RemoveBG_API = get_str_key("REM_BG_API_KEY", required=False)
 
