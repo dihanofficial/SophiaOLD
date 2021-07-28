b50007e6349277a8288e6647fa9b193cc9bcf6eb
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-07-28 05:42:51 +0530

    Sophia

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -660,8 +660,6 @@ def main():
     # dispatcher.add_handler(test_handler)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(help_handler)
-    dispatcher.add_handler(about_callback_handler)
-    dispatcher.add_handler(source_callback_handler)
     dispatcher.add_handler(settings_handler)
     dispatcher.add_handler(help_callback_handler)
     dispatcher.add_handler(settings_callback_handler)
