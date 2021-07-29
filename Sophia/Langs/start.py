a5774418635e4170567797c1104845797e6c8aec
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-07-29 06:45:16 +0530

    Create start.py

---
+++ Sophia/Langs/start.py
@@ -0,0 +1,20 @@
+ __lang__ = "en"
+
+en = { 
+
+  # __main__
+"start_text": """
+    
+pm_menu:
+    start_hi_group: 'Hey there! My name is Sophia
+    start_hi: "Hey there! My name is <b>Sophia.</b>\nI can help manage your groups with useful features, feel free to add me to your groups!"
+    btn_source: "📜 Source code"
+    btn_help: "❔ Help"
+    btn_lang: "🇺🇸 Language"
+    btn_channel: "🙋‍♀️ Sophia News"
+    btn_group: "👥 Sophia Support"
+    btn_group_help: "Click me for help!"
+    back: "🏃‍♂️ Back"
+
+    # /help
+    help_header: "Hi Boss! I'm <b>Sophia</b>. An anime themed super powerful group management bot with many handy tools. So why are you waiting. Let me to assist you"
