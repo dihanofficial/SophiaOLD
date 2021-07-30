1c8f1cdee079c27552e27a269a0bcba077c4b002
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-07-30 14:47:19 +0530

    Sophia

--- Sophia/__main__.py
+++ Sophia/__main__.py
@@ -425,25 +425,26 @@ def Sophia_info_callback(update, context):
     query = update.callback_query
     if query.data == "info_":
         query.message.edit_text(
-            text=""" I'm *Sophia*, a Powerful Group Management Bot built to Help you manage your group Easily.
+            text="""f"\n\n I'm *Sophia*, a Powerful Group Management Bot built to Help you manage your group Easily.
 
-                 f"\n* I can restrict users.
+                 f"\n\n* I can restrict users.
 
-                 f"\n* I can greet users with customizable welcome messages and even set a group's rules.
+                 f"\n\n* I can greet users with customizable welcome messages and even set a group's rules.
 
-                 f"\n* I have an advanced anti-flood system.
+                 f"\n\n* I have an advanced anti-flood system.
 
-                 f"\n* I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
+                 f"\n\n* I can warn users until they reach max warns, with each predefined actions such as ban, mute, kick, etc.
 
-                 f"\n* I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
+                 f"\n\n* I have a note keeping system, blacklists, and even predetermined replies on certain keywords.
 
-                 \n* I check for admins' permissions before executing any command and more stuffs
+                 f"\n\n* I check for admins' permissions before executing any command and more stuffs
 
-                 \n\n_LSophia's licensed under the GNU General Public License v3.0_
+                 f"\n\n  Sophia's licensed under the GNU General Public License v3.0
 
-                 \n- Support Group @Dihan_Official
-                 \n- Assistant @SophiaXMusic.
-                 \n\nIf you have any question about Sophia, let us know at .""",
+                 f"\n\n - Support Group @Dihan_Official
+                 f"\n\n - Assistant @SophiaXMusic.
+
+                f"\n\n If you have any question about Sophia, let us know at .""",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
