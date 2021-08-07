f4ae6d3b82d2aa77a0140e9d23574d5c98d7e4fb
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-08-07 13:07:43 +0530

    Update broadcast.py

--- Sophia/modules/broadcast.py
+++ Sophia/modules/broadcast.py
@@ -6,7 +6,7 @@ import datetime
 import aiofiles
 import asyncio
 import traceback
-from Sophia.conf import Config
+
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 
 broadcast_ids = {}
