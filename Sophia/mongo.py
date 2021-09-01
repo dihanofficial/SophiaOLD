09923ba07a7836724cf576cda61126da8c6a3244
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-09-01 05:32:10 +0530

    Update mongo.py

--- Sophia/mongo.py
+++ Sophia/mongo.py
@@ -10,7 +10,7 @@ from Sophia.conf import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "Sophia"
+MONGO_DB = "DaisyX"
 
 
 client = MongoClient()
