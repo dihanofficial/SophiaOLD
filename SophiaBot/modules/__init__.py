1c40a613ac295ebe8a077c6a506223015dc4474f
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-10-08 20:56:02 +0530

    Sophia

--- SophiaBot/modules/__init__.py
+++ SophiaBot/modules/__init__.py
@@ -5,6 +5,7 @@ def __list_all_modules():
     import glob
     from os.path import basename, dirname, isfile
 
+
     # This generates a list of modules in this folder for the * in __main__ to work.
     mod_paths = glob.glob(dirname(__file__) + "/*.py")
     all_modules = [
