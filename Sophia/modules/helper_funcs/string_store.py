e9809583367dec24407bd9dcbfdccaa24f54addd
Dihan Official <84424959+dihanofficial@users.noreply.github.com>
2021-10-10 12:36:32 +0530

    Sophia

---
+++ Sophia/modules/helper_funcs/string_store.py
@@ -0,0 +1,43 @@
+GDNIGHT = [
+    "Good night!",
+]
+
+GDMORNING = [
+    "Good morning!",
+]
+
+SOPHIA = [
+     "Something went wrong.",
+     "Something went wrong.",
+     "Nope.",
+     "Nope.",
+     "Nope.",
+     "Nope.",
+     "Nope.",
+     "Something went wrong.",
+     "Maybe.",
+     "Something went wrong.",
+     "Something went wrong.",
+     "Nope.",
+     "Nope.",
+     "Nope.",
+     "Nope.",
+     "Nope.",
+     "Something went wrong.",
+     "Maybe.",
+     "Something went wrong.",
+     "Something went wrong.",
+     "Nope.",
+     "Nope.",
+     "Nope.",
+     "Nope.",
+     "Nope.",
+     "Something went wrong.",
+     "Maybe.",
+]
+
+
+
+
+
+
